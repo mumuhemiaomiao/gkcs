14ecba01b18be83038861c8e155ba446a52a1eb4
mumuhemiaomiao <973501963@qq.com>
2023-07-11 21:03:47 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 const $ = (id) => document.getElementById(id)
 const $$ = (classname) => document.getElementsByClassName(classname)
 $('music_icon').onclick = ()=>{
-	music()
-}
-function music() {
-	$('music_icon').style.display = "block"
-	if ($('bgm').paused) {
+		if ($('bgm').paused) {
 		$('bgm').play()
 		$('music_icon').src = "img/music_on.png"
 		$('music_icon').style.animationPlayState = "running"
@@ -15,3 +11,17 @@ function music() {
 		$('music_icon').style.animationPlayState = "paused"
 	}
 }
+// function music() {
+// 	$('music_icon').style.display = "block"
+// 	if ($('bgm').paused) {
+// 		$('bgm').play()
+// 		$('music_icon').src = "img/music_on.png"
+// 		$('music_icon').style.animationPlayState = "running"
+// 	} else{
+// 		$('bgm').pause()
+// 		$('music_icon').src = "img/music_off.png"
+// 		$('music_icon').style.animationPlayState = "paused"
+// 	}
+// }
+
+
