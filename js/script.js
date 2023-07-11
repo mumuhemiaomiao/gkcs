08a7884c092274dfc3ed98f0af2fb3586fe439b2
mumuhemiaomiao <973501963@qq.com>
2023-07-11 17:29:00 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const $ = (id) => document.getElementById(id)
+const $$ = (classname) => document.getElementsByClassName(classname)
+$('music_icon').onclick = ()=>{
+	music()
+}
+function music() {
+	$('music_icon').style.display = "block"
+	if ($('bgm').paused) {
+		$('bgm').play()
+		$('music_icon').src = "img/music_on.png"
+		$('music_icon').style.animationPlayState = "running"
+	} else{
+		$('bgm').pause()
+		$('music_icon').src = "img/music_off.png"
+		$('music_icon').style.animationPlayState = "paused"
+	}
+}
