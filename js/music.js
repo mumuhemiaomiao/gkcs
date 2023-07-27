3d1fd490feedf7e8ad4bfc41ba9ecc5fb5f4c466
mumuhemiaomiao <973501963@qq.com>
2023-07-27 15:28:21 +0800

    Create music.js

---
+++ js/music.js
@@ -0,0 +1,12 @@
+function p1(){
+    var t=document.body;
+    t.innerHTML=`
+    <div id="content">
+		<div id="view">
+		</div>
+	</div>
+    <img src="./img/music_off.png" id="music_icon">
+	<audio src="./madie/BGM.mp3" id="bgm" loop="1" ></audio>
+`
+}
+p1()
