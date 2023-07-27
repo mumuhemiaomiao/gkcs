9b5a55faf2aa0cf82d4d05012b36ae39be7a8a97
mumuhemiaomiao <973501963@qq.com>
2023-07-27 15:25:51 +0800

    Update meng.js

--- js/meng.js
+++ js/meng.js
@@ -56,31 +56,75 @@ function p3F() {
 	}
 	init()
 	// 判断对错
+	var score = 0
+	// 是否可以点击的标记
+	var isClickable = true;
 	for (let i = 0; i < $$('opt').length; i++) { 
 		$$('opt').item(i).onclick = ()=>{
-			// 当前选择的题目索引是否等于答案数组里的题目索引
+			if(!isClickable)return;
+			isClickable = false;
 			if (i === ans_index[tiIndex]) {
-				console.log('对');
-				// 空白的题目索引图片等于点击的图片
+			// 	console.log('对');
+			score +=20;
+			}
+			let left = $$('opt').item(i).offsetLeft
+			let top = $$('opt').item(i).offsetTop
+			let width = $$('opt').item(i).width
+			let height = $$('opt').item(i).height
+			$$('opt').item(i).style.left = $$('kong').item(tiIndex).offsetLeft + 'px';
+			$$('opt').item(i).style.top = $$('kong').item(tiIndex).offsetTop + 'px';
+			$$('opt').item(i).style.width = $$('kong').item(tiIndex).width + 'px';
+			$$('opt').item(i).style.height = $$('kong').item(tiIndex).height + 'px';
+			setTimeout(()=>{
 				$$('opt').item(i).style.display = "none"
+				$$('opt').item(i).style.left = left + 'px';
+				$$('opt').item(i).style.top = top + 'px';
+				$$('opt').item(i).style.width = width + 'px';
+				$$('opt').item(i).style.height = height + 'px';
+				console.log(left);
 				$$('kong').item(tiIndex).src = $$('opt').item(i).src
+				isClickable = true;
 				tiIndex++
-				addClass(tiIndex)
 				if (tiIndex == 5) {
 					$('ok').style.display = "block"
+					//显示得分
+					result.innerHTML = "您的得分是："+score+"分"
+					//渲染饼图
+					generatePieChart(score,100)
 					$('ok').onclick = ()=>{
 						$('ok').style.display = "none"
+						// 移除饼图容器元素
+						// chartContainer.parentNode.removeChild(chartContainer);
 						p2F()
 						init()
 					}
 				}
-			} else{
-				console.log('错');
-				$('err').style.display = "block"
-				$('p3_err').onclick = ()=>{
-					$('err').style.display = "none"
-				}
-			}
+				addClass(tiIndex)
+			},600)
+			// 当前选择的题目索引是否等于答案数组里的题目索引
+			// if (i === ans_index[tiIndex]) {
+			// 	console.log('对');
+			// 	score +=20;
+			// 	// 空白的题目索引图片等于点击的图片
+			// 	$$('opt').item(i).style.display = "none"
+			// 	$$('kong').item(tiIndex).src = $$('opt').item(i).src
+			// 	tiIndex++
+			// 	addClass(tiIndex)
+			// 	if (tiIndex == 5) {
+			// 		$('ok').style.display = "block"
+			// 		$('ok').onclick = ()=>{
+			// 			$('ok').style.display = "none"
+			// 			p2F()
+			// 			init()
+			// 		}
+			// 	}
+			// } else{
+			// 	console.log('错');
+			// 	$('err').style.display = "block"
+			// 	$('p3_err').onclick = ()=>{
+			// 		$('err').style.display = "none"
+			// 	}
+			// }
 		}
 	}
 }
